871172833c55f193f59cd862ccdc1b75eae4f1dd
Mike Redko <30830926+somerandomdev49@users.noreply.github.com>
2020-08-27 21:36:47 +0300

    hello, python (compiler)

--- mecc.py
+++ mecc.py
@@ -10,11 +10,64 @@ INSTR = [
     "CLL", # CaLL a function.
     "PAU", # PAUse
     "PUT", # PUT a string to the console.
-    "ADD", "SUB", "MUL", "DIV", "AND", "COR", "NOT", "CLT", "CGT", "CLE", "CGE",
     "JMP", # JuMP to immediate instruction.
     "CJP", # Conditional JumP (if register 0 is truthy then jump).
+    "ADD", "SUB", "MUL", "DIV", "AND", "COR", "NOT", "CLT", "CGT", "CLE", "CGE",
 ]
 
+print(INSTR.index("JMP"))
+
 f = open("test.mec", "r")
-data = f.read()
+
+vals = []
+inst = []
+
+import shlex
+i = -1
+for line in f.readlines():
+    i += 1
+    line = line.strip()
+    if len(line) == 0: continue
+    if line[0] == ';': continue
+    print(str(i) + " |", line)
+
+    toks = shlex.split(line)
+    if toks[0] == "VAL":
+        vals.append(toks[1])
+        continue
+    if toks[0] not in INSTR:
+        print("Unknown instruction: '" + toks[0] + "'")
+    
+    args = []
+    if len(toks) < 2:
+        args = []
+    if len(toks) == 2:
+        args.append(int(toks[1]))
+    if len(toks) == 3:
+        args.append(int(toks[1]))
+        args.append(int(toks[2]))
+    inst.append({ "name": INSTR.index(toks[0]), "args": args})
+print(vals)
+print(inst)        
 f.close()
+
+bin_out = bytearray([])
+bin_out.extend(len(vals).to_bytes(length=2, byteorder="big"))
+for val in vals:
+    for c in val:
+        bin_out.append(ord(c))
+    bin_out.append(0)
+for ins in inst:
+    bin_out.append(ins["name"])
+    if len(ins["args"]) == 0:
+        bin_out.append(0)
+        bin_out.append(0)
+    if len(ins["args"]) == 1:
+        bin_out.extend(ins["args"][0].to_bytes(length=2, byteorder="big"))
+    if len(ins["args"]) == 2:
+        bin_out.append(ins["args"][0])
+        bin_out.append(ins["args"][1])
+
+out = open("out.mecc", "bw")
+out.write(bin_out)
+out.close()
